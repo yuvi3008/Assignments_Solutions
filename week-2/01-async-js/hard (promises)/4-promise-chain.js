227c24576458bee43e5c5979ae56da4bfb10c774
Yuvraj Singh Thakur <yuvrajthakur30@gmail.com>
2023-12-12 23:31:27 +0530

    Assignments solved week 2

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,18 +5,106 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function waitOneSecond() {
+/*
+Promise chain
+Executes one by one 
+(synchronous).
+● Result of one execution 
+can be passed to the 
+next step.
+● Execution time to 
+fulfill all the promise 
+is comparatively high.
+● Chaining after an error 
+is possible.
 
+Promise.all
+● Executes in parallel 
+(asynchronous).
+● Result of one promise 
+cannot be passed to an 
+another promise.
+● Parallel execution 
+guarantees in reduced 
+time.
+● It rejects with the 
+reason of the first 
+promise that rejects.
+
+
+*/
+
+function waitOneSecond() {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },1000);
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },2000);
+    })
 }
 
 function waitThreeSecond() {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },3000);
+    })
 
 }
 
-function calculateTime() {
+async function calculateforNseconds(n){   
+    return new Promise(async (resolve)=>{
+        let three,two,one;
+        /*
+        Consider you have to wait for 10 seconds, then 
+        it means 3 calls to waitThreeSecond() and one call to waitTwoSeconds().
+        */
+        
+        three = parseInt(n/3);
+        if(n%3 == 2){
+            two = true;
+        }else if(n%3 == 1){
+            one = true; 
+        }
+        for(let i=0;i<three;i++){
+            await waitThreeSecond();
+        }
+        if(two){
+            await waitTwoSecond();
+        } else if(one){
+            await waitOneSecond();
+        }
+        // resolve(`Resolved for ${n} seconds`);
+        resolve();
+    })
+    
 
 }
+async function calculateTime(a,b,c) {
+
+    let start = Date.now();
+    await calculateforNseconds(a)
+    .then((resolve)=>{
+        return calculateforNseconds(b);
+    })
+    .then(()=>{
+        return calculateforNseconds(c);
+    })
+    end = Date.now();
+    return end - start;
+}
+
+
+
+
+
+ console.log(calculateTime(3,4,1));
+
+module.exports = calculateTime;
